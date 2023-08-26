074f1bce44a2a74933e12cad4ea43c23e73a4c5a
willyjo1 <arthurwilchield@gmail.com>
2023-08-26 10:30:52 +0000

    completed my portfolio for web

--- lib/view/follow_up_page.dart
+++ lib/view/follow_up_page.dart
@@ -1,25 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../global_variables/colors.dart';
 
-class FollowUpPage extends StatelessWidget {
+class FollowUpPage extends StatefulWidget {
   const FollowUpPage({super.key});
 
   @override
+  State<FollowUpPage> createState() => _FollowUpPageState();
+}
+
+class _FollowUpPageState extends State<FollowUpPage> {
+  List<String> uriLinks = [
+    "https://medium.com/@arthurwilchield",
+    "https://www.linkedin.com/in/jonathan-arthur93b239228",
+    "@wilchield",
+    "https://github.com/jocypher"
+  ];
+  void _launchURL(String url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      print('Could not launch $url');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-       Text("Follow Me ",style: TextStyle(color: headingElementTheme, fontSize: 30 , fontWeight: FontWeight.bold),),
-       const SizedBox(height: 30,),
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
+        "Follow Me ",
+        style: TextStyle(
+            color: headingElementTheme,
+            fontSize: 30,
+            fontWeight: FontWeight.bold),
+      ),
+      const SizedBox(
+        height: 30,
+      ),
       Row(
-        
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-        Image.asset("assets/icons8-medium-96.png",height: 70, width: 50, ),
-        Image.asset("assets/icons8-github-logo-96.png",height: 70, width: 50,),
-        Image.asset("assets/icons8-twitter-96.png",height: 70, width: 50,)
-      ],)
+          InkWell(
+            onTap: () {
+              _launchURL(uriLinks[0]);
+            },
+            child: Image.asset(
+              "assets/icons8-medium-96.png",
+              height: 70,
+              width: 50,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              _launchURL(uriLinks[3]);
+            },
+            child: Image.asset(
+              "assets/icons8-github-logo-96.png",
+              height: 70,
+              width: 50,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              _launchURL(uriLinks[2]);
+            },
+            child: Image.asset(
+              "assets/icons8-twitter-96.png",
+              height: 70,
+              width: 50,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              _launchURL(uriLinks[1]);
+            },
+            child: Image.asset(
+              "assets/icons8-linkedin-96.png",
+              height: 70,
+              width: 50,
+            ),
+          )
+        ],
+      )
     ]);
   }
 }
--- lib/view/my_projects.dart
+++ lib/view/my_projects.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_project/global_variables/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyProjectsPage extends StatefulWidget {
   const MyProjectsPage({super.key});
@@ -14,6 +15,21 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
   bool isProj3Hover = false;
   bool isProj4Hover = false;
 
+  List<String> uriLinks = [
+    "https://github.com/Polymorph-Labs-Interns/Studii-Mobile",
+    "https://github.com/jocypher/Veterinary-Application",
+    "https://github.com/jocypher/fast_food_design",
+    "https://github.com/jocypher/fast_food_design"
+  ];
+
+  void _launchURL(String url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      print('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -23,27 +39,48 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
         padding: const EdgeInsets.only(left: 50, right: 50),
         child: Column(
           children: [
-             Padding(
-               padding: const EdgeInsets.only(top:30.0),
-               child: Text("My Projects", style: TextStyle(fontSize: 30, color: headingElementTheme, fontWeight: FontWeight.bold)),
-             ),
-            const Text("Projects worked on and developed", style: TextStyle(fontSize: 20),),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Text("My Projects",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: headingElementTheme,
+                      fontWeight: FontWeight.bold)),
+            ),
+            const Text(
+              "Projects worked on and developed",
+              style: TextStyle(fontSize: 20),
+            ),
             SizedBox(height: height * 0.1),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset("assets/icons8-android-os-48.png"),
-                 Column(
+                Expanded(
+                  child: Image.asset(
+                    "assets/Normal.png",
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Veterinary Resource Application", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
-                    const Text("Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application.", style: TextStyle(letterSpacing: 1),),
+                    const Text(
+                      "Studii Application",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    const Text(
+                      "Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application.",
+                      style: TextStyle(letterSpacing: 1),
+                    ),
                     const SizedBox(height: 10),
                     AnimatedContainer(
                       duration: const Duration(seconds: 2),
                       child: InkWell(
                         onTap: () {
-                          
+                          _launchURL(uriLinks[0]);
                         },
                         onHover: (value) {
                           setState(() {
@@ -54,32 +91,43 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
                           height: 50,
                           width: 130,
                           decoration: BoxDecoration(
-                            color: isVetHover ? headingElementTheme : Colors.white,
-                            border: Border.all(color: isVetHover ? Colors.black: headingElementTheme)
-                          ),
-                          child: const Center(child: Text('View Code')),),
+                              color: isVetHover
+                                  ? headingElementTheme
+                                  : Colors.white,
+                              border: Border.all(
+                                  color: isVetHover
+                                      ? Colors.black
+                                      : headingElementTheme)),
+                          child: const Center(child: Text('View Code')),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ],
             ),
-            const SizedBox(height: 100,),
+            const SizedBox(
+              height: 100,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                 Column(
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Veterinary Resource Application", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
-                    const Text("Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application."),
-    
+                    const Text(
+                      "Veterinary Resource Application",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    const Text(
+                        "Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application."),
                     const SizedBox(height: 10),
                     AnimatedContainer(
                       duration: const Duration(seconds: 2),
                       child: InkWell(
                         onTap: () {
-                          
+                          _launchURL(uriLinks[2]);
                         },
                         onHover: (value) {
                           setState(() {
@@ -90,33 +138,55 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
                           height: 50,
                           width: 130,
                           decoration: BoxDecoration(
-                            color: isProj2Hover ? headingElementTheme : Colors.white,
-                            border: Border.all(color: isProj2Hover ? Colors.black: headingElementTheme)
-                          ),
-                          child: const Center(child: Text('View Code')),),
+                              color: isProj2Hover
+                                  ? headingElementTheme
+                                  : Colors.white,
+                              border: Border.all(
+                                  color: isProj2Hover
+                                      ? Colors.black
+                                      : headingElementTheme)),
+                          child: const Center(child: Text('View Code')),
+                        ),
                       ),
                     ),
                   ],
                 ),
-                Image.asset("assets/icons8-android-os-48.png"),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(child: Image.asset("assets/fooddesign.jpeg")),
               ],
             ),
-            const SizedBox(height: 100,),
-             Row(
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset("assets/icons8-android-os-48.png"),
-                 Column(
+                Expanded(
+                  child: Image.asset(
+                    "assets/B7473E35-200E-4B29-8B63-1BE1AA463D66.jpeg",
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Veterinary Resource Application", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
-                    const Text("Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application."),
+                    const Text(
+                      "Veterinary Resource Application",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    const Text(
+                        "Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application."),
                     const SizedBox(height: 10),
                     AnimatedContainer(
                       duration: const Duration(seconds: 2),
                       child: InkWell(
                         onTap: () {
-                          
+                          _launchURL(uriLinks[1]);
                         },
                         onHover: (value) {
                           setState(() {
@@ -127,31 +197,43 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
                           height: 50,
                           width: 130,
                           decoration: BoxDecoration(
-                            color: isProj3Hover ? headingElementTheme : Colors.white,
-                            border: Border.all(color: isProj3Hover ? Colors.black: headingElementTheme)
-                          ),
-                          child: const Center(child: Text('View Code')),),
+                              color: isProj3Hover
+                                  ? headingElementTheme
+                                  : Colors.white,
+                              border: Border.all(
+                                  color: isProj3Hover
+                                      ? Colors.black
+                                      : headingElementTheme)),
+                          child: const Center(child: Text('View Code')),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ],
             ),
-            const SizedBox(height: 100,),
+            const SizedBox(
+              height: 100,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                 Column(
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Veterinary Resource Application", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
-                    const Text("Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application."),
+                    const Text(
+                      "Veterinary Resource Application",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    const Text(
+                        "Built an app for School of Veterinary Medicine students,offering access to educational resources,\nnews updates, and easy contacts to lectures.Features include categorized study materials, announcements,\nfeedback channels, secure authentication,user profiles, notifications events calendar and slides section.\nPrioritize data security, usability, and cross-platform compatibility and a feedback section \nto get user response on the application."),
                     const SizedBox(height: 10),
                     AnimatedContainer(
                       duration: const Duration(seconds: 2),
                       child: InkWell(
                         onTap: () {
-                          
+                          _launchURL(uriLinks[3]);
                         },
                         onHover: (value) {
                           setState(() {
@@ -162,19 +244,28 @@ class _MyProjectsPageState extends State<MyProjectsPage> {
                           height: 50,
                           width: 130,
                           decoration: BoxDecoration(
-                            color: isProj4Hover ? headingElementTheme : Colors.white,
-                            border: Border.all(color: isProj4Hover ? Colors.black: headingElementTheme)
-                          ),
-                          child: const Center(child: Text('View Code')),),
+                              color: isProj4Hover
+                                  ? headingElementTheme
+                                  : Colors.white,
+                              border: Border.all(
+                                  color: isProj4Hover
+                                      ? Colors.black
+                                      : headingElementTheme)),
+                          child: const Center(child: Text('View Code')),
+                        ),
                       ),
                     ),
-                      const SizedBox(height: 20,)
+                    const SizedBox(
+                      height: 20,
+                    )
                   ],
                 ),
-                Image.asset("assets/icons8-android-os-48.png"),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(child: Image.asset("assets/instagram_clone.jpeg")),
               ],
             ),
-    
           ],
         ),
       ),
